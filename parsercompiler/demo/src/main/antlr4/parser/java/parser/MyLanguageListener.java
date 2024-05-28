dfe86aa52d84cd1e1f117a00f891dbfc23f09e25
Ricardo André <91620783+rickzerahh@users.noreply.github.com>
2024-05-28 00:35:50 -0300

    Update MyLanguageListener.java

--- parsercompiler/demo/src/main/antlr4/parser/java/parser/MyLanguageListener.java
+++ parsercompiler/demo/src/main/antlr4/parser/java/parser/MyLanguageListener.java
@@ -3,13 +3,11 @@ package parser;
 public class MyLanguageListener extends LanguageBaseListener {
     @Override
     public void enterStart(LanguageParser.StartContext ctx) {
-        // Implemente a lógica para processar a entrada da regra 'start' aqui
         System.out.println("Entrou na regra 'start'");
     }
 
     @Override
     public void exitStart(LanguageParser.StartContext ctx) {
-        // Implemente a lógica para processar a saída da regra 'start' aqui
         System.out.println("Saiu da regra 'start'");
     }
 }
